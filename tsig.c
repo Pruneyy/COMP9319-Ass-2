9e13fb8bc22a7c75356d5a7ff79bd8bf2599e5e5
Pruneyy <z3461857@student.unsw.edu.au>
2018-10-17 09:28:10 +1100

    Added codeword function

--- tsig.c
+++ tsig.c
@@ -10,11 +10,45 @@
 #include "hash.h"
 #include "bits.h"
 
+/*
+bits codeword(char *attr_value, int m, int k)
+{
+    int nbits = 0; // count of set bits
+    bits cword = 0; // assuming m <= 32 bits
+    srandom(hash(attr_value));
+    while (nbits < k) {
+        int i = random() % m;
+        if (((1 << i) & cword) == 0) {
+            cword |= (1 << i);
+            nbits++;
+        }
+    }
+    return cword; // m-bits with k 1-bits and m-k 0-bits
+}
+*/
+
+Bits codeword(char *attrValue, int m, int k)
+{
+    int nBits = 0;
+    //It says assuming m < 32 bits, will this always be true?
+    Bits cWord = newBits(m);
+    srandom(hash_any(attrValue, strlen(attrValue)));
+    while (nBits < k) {
+        int i = random() % m;
+        if (!bitIsSet(cWord, i)) {
+            setBit(cWord, i);
+            nBits++;
+        }
+    }
+    return cWord;
+}
+
 // make a tuple signature
 
 Bits makeTupleSig(Reln r, Tuple t)
 {
 	assert(r != NULL && t != NULL);
+
 	//TODO
 	return NULL;
 }
