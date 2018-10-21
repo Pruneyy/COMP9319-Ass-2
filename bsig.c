ebe8b75c48dd86843cde77cbd85cd469400fc7f0
Chris Armstrong <chrisjarmstrong97@gmail.com>
2018-10-21 11:12:19 +1100

    findPagesUsingBitSlices drafted

--- bsig.c
+++ bsig.c
@@ -6,6 +6,7 @@
 #include "reln.h"
 #include "query.h"
 #include "bsig.h"
+#include "psig.h"
 
 /*
 Qsig = makePageSig(Query)
@@ -17,10 +18,39 @@ for each i in 0..pm-1 {
     }
 }
 */
-void findPagesUsingBitSlices(Query q)
+void findPagesUsingBitSlices(Query q) //TODO
 {
 	assert(q != NULL);
-	//TODO
-	setAllBits(q->pages); // remove this
+	// Get query Relation
+    Reln r = q->rel;
+    
+	// Get query Bits
+	Bits querySig = makePageSig(q->rel,q->qstring);
+	
+    // Full bitString to store Page
+    Bits pages = q->pages;
+    setAllBits(pages);
+    
+    Bits slice = newBits(bsigBits(r));
+	Count j;
+	Count bspage = 0;
+    Count bitnum;
+	Page pb = getPage(bsigFile(r),bspage);
+	for (j = 0;j < psigBits(r);j++) {
+		if (j % maxBsigsPP(r) == 0) {
+			pb = getPage(bsigFile(r),bspage);
+		}
+		if (bitIsSet(querySig,j)) {
+			getBits(pb,(j%maxBsigsPP(r))*bsigBits(r),slice);
+            for (bitnum = 0;bitnum < bsigBits(r);bitnum++) {
+                if (!bitIsSet(slice,bitnum)) {
+                    unsetBit(pages, bitnum);
+                }
+            }
+		}
+	}
+    // The printf below is primarily for debugging
+	// Remove it before submitting this function
+	printf("Matched Pages:"); showBits(q->pages); putchar('\n');
 }
 
